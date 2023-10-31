7adcb090935ecc4cb1ac871c36507286e9dbb67e
Fabio Marsiaj <fabiobackend@gmail.com>
2023-10-31 11:36:12 -0300

    Func Carregar

---
+++ api/src/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// String de conexão do banco MySQL
+	StringConexaoBanco = ""
+
+	// Port da API
+	Porta = 0
+)
+
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"))
+}
